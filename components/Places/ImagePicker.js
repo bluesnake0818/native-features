4f12b297f148157a86cb294eec53118f1707fa0e
bluesnake0818 <raidient87@gmail.com>
2022-05-29 13:20:12 +0900

    add custom button

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,4 +1,4 @@
-import { Alert, Button, View, StyleSheet, Text, Image } from 'react-native'
+import { Alert, View, StyleSheet, Text, Image } from 'react-native'
 import {
   launchCameraAsync,
   useCameraPermissions,
@@ -7,6 +7,7 @@ import {
 import { useState } from 'react'
 
 import { Colors } from '../../constants/colors'
+import OutlinedButton from '../UI/OutlinedButton'
 
 const ImagePicker = () => {
   const [pickedImage, setPickedImage] = useState()
@@ -57,7 +58,7 @@ const ImagePicker = () => {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>Take Image</OutlinedButton>
     </View>
   )
 }
